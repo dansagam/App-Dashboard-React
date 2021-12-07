8e8a93a569a8776ae94603970f7817e898035359
dansagam <olukayodeogunnowo@yahoo.com>
2021-12-07 18:15:24 +0100

    create the various card component

--- src/components/CustomTabs/CustomTabs.jsx
+++ src/components/CustomTabs/CustomTabs.jsx
@@ -1,8 +1,17 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import { experimentalStyled as styled, makeStyles } from '@mui/material/styles'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import styles from './customTabStyle'
 
 const CustomTabs = (props) => {
-   // eslint-disable-next-line no-unused-vars
-   const { name } = props
+   const [value, setValue] = useState(0)
+   const handleChange = (event, valu) => {
+      setValue(valu)
+   }
+   const { headerColor, plainTabs, tabs, title, rtlActive } = props
    // thjbksdjksajdad
    return (
       <div>
@@ -11,7 +20,24 @@ const CustomTabs = (props) => {
    )
 }
 CustomTabs.propTypes = {
-   name: PropTypes.bool,
+   headerColor: PropTypes.oneOf([
+      'warning',
+      'success',
+      'danger',
+      'info',
+      'primary',
+      'rose',
+   ]),
+   title: PropTypes.string,
+   tabs: PropTypes.arrayOf(
+      PropTypes.shape({
+         tabName: PropTypes.string.isRequired,
+         tabIcon: PropTypes.object,
+         tabContent: PropTypes.node.isRequired,
+      })
+   ),
+   rtlActive: PropTypes.bool,
+   plainTabs: PropTypes.bool,
 }
 
 export default CustomTabs
